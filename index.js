af8b196629ca209611fbb3d4268d11c0c5c94a1b
Kasembeli8865 <lionel.kasembeli@student.moringaschool.com>
2023-06-15 15:14:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution here!
+const cats =['Milo', 'Otis','Garfield'];
+
+const destructivelyAppendCat = cats.push('Ralph');
+
+const destructivelyPrependCat = cats.unshift('Bob');
+
+const destructivelyRemoveLastCat = cats.pop();
+
+const appendCat =[...cats, 'Broom'];
+
+const prependCat = ['Arnold', ...cats];
+
+const removeLastCat = cats.slice(3);
+
+const removeFirstCat = cats.slice(0)
+
